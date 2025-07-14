66f4d650c6d74f0d25199039989b01a608cadcde
Artem Horbunov <artemohorbunov@gmail.com>
2025-07-14 17:14:18 -0500

    feat: set up Tic‑Tac‑Toe game loop and input handling
    - Added TicTacToeRunner with welcome prompt, symbol selection, and main loop
    - Stubbed Board.isFull() and Board.checkWin(); to be implemented next

--- Board.java
+++ Board.java
@@ -1,10 +1,74 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Board {
-	public void displayBoard() {
+	private ArrayList<Character> board = new ArrayList<>(Arrays.asList('1', '2', '3', '4', '5', '6', '7', '8', '9'));
+	private char computersSymbol;
+	private int random;
 
+	public void displayBoard() {
+		System.out.println();
+		for (int i = 0; i < 3; i++) { // 3 rows
+			for (int j = 0; j < 3; j++) {
+				System.out.print(" " + board.get(i * 3 + j));
+				if (j < 2)
+					System.out.print(" |");
+			}
+			System.out.println();
+			if (i < 2) {
+				System.out.println("-----------");
+			}
+		}
 	}
 
-	public void updateBoard() {
+	public boolean checkWin() { // DO NEXT
+		for (int i = 0; i < board.size(); i++) {
 
+		}
+		return false;
+	}
+
+	public boolean isOpen(int position) {
+		if (board.get(position) == 'X' || board.get(position) == 'O') {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	public boolean isFull() { // DO NEXT
+		return false;
+	}
+	
+	public boolean makeMove(int position, char symbol) {
+	    if (isOpen(position)) {
+	        board.set(position, symbol);
+	        return true;                 // move accepted
+	    } else {
+	        System.out.println("\nThat square is already filled!");
+	        return false;                // move rejected
+	    }
+	}
+
+
+	public void computersMove(char opponentsSymbol) { // Randomly pick an index from board and make a move if it's open
+		if (opponentsSymbol == 'X') {
+			computersSymbol = 'O';
+		} else {
+			computersSymbol = 'X';
+		}
+		do {
+			random = (int) (Math.random() * board.size()); // Generate a random number 0-8
+			isOpen(random);
+		} while (!isOpen(random));
+		makeMove(random, computersSymbol);
+	}
+
+	public ArrayList<Character> getBoard() {
+		return board;
+	}
+
+	public void setBoard(ArrayList<Character> board) {
+		this.board = board;
 	}
 }
--- TicTacToeRunner.java
+++ TicTacToeRunner.java
@@ -3,11 +3,25 @@ import java.util.Scanner;
 public class TicTacToeRunner {
 
 	public static void main(String[] args) {
-		Scanner user = new Scanner(System.in);
-		System.out.println("Welcome to Tic Tac Toe");
-		
-		System.out.print("Enter a position from 1-9: ");
-		int guess = user.nextInt();
+		Board board = new Board();
+		Scanner input = new Scanner(System.in);
+		System.out.print("Welcome to Tic Tac Toe!");
+		char playerSymbol;
+		do {
+			System.out.print("\nWould you like to play for X or O ?: ");
+			playerSymbol = input.next().charAt(0);
+			playerSymbol = Character.toUpperCase(playerSymbol);
+		} while (playerSymbol != 'X' && playerSymbol != 'O');
+		do {
+			board.displayBoard();
+			System.out.print("\nEnter a position from 1-9: ");
+			int position = input.nextInt();
+			position--;
+			boolean userMoved = board.makeMove(position, playerSymbol); // User move
+			if (userMoved) { // Only let PC make a move if a user successfully moved
+				board.computersMove(playerSymbol); // PC move
+			}
+		} while (!board.checkWin());
 
 	}
 
