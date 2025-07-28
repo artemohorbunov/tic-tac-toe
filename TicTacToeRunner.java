dc7a53735918589d2619d3b7096d02b73c24f65d
Artem Horbunov <artemohorbunov@gmail.com>
2025-07-28 18:37:24 -0500

    fix: prevent crash on invalid input during user move prompt

--- TicTacToeRunner.java
+++ TicTacToeRunner.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TicTacToeRunner {
@@ -9,6 +10,7 @@ public class TicTacToeRunner {
 		char playerSymbol;
 		boolean win;
 		boolean full;
+		boolean userMoved = false;
 		do {
 			System.out.print("\nWould you like to play for X or O ?: ");
 			playerSymbol = input.next().charAt(0);
@@ -16,16 +18,22 @@ public class TicTacToeRunner {
 		} while (playerSymbol != 'X' && playerSymbol != 'O');
 		do {
 			board.displayBoard();
-			System.out.print("\nEnter a position from 1-9: ");
-			int position = input.nextInt();
-			boolean userMoved = board.makeMove(position, playerSymbol); // User move
+			try {
+				System.out.print("\nEnter a position from 1-9: ");
+				int position = input.nextInt();
+				userMoved = board.makeMove(position, playerSymbol); // User move
+				// If user inputs anything other than numbers 1-9 this will catch it
+			} catch (InputMismatchException | IndexOutOfBoundsException e) {
+				System.out.println("\nOnly numbers from 1-9!");
+				input.nextLine(); // Clear the buffer to avoid an infinite loop
+			}
 			win = board.checkWin(playerSymbol); // Check if there's a win
 			full = board.isFull(); // Check if board is full
 			if (full && !win) {
 				System.out.println("Draw!");
 			}
 			if (win) {
-				System.out.println("\nPlayer " + playerSymbol + " WON!" );
+				System.out.println("\nPlayer " + playerSymbol + " WON!");
 				break;
 			}
 			// Let PC move if a user successfully moved and there isn't a win or full-board
